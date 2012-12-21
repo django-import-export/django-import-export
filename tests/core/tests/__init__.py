26e476287e1aefff89c5a5586a6d23dea6c34ca3
Bojan Mihelac <bmihelac@mihelac.org>
2012-12-21 10:27:19 +0100

    Add admin integration tests

--- tests/core/tests/__init__.py
+++ tests/core/tests/__init__.py
@@ -2,3 +2,4 @@ from fields_tests import *
 from widgets_tests import *
 from resources_tests import *
 from instance_loaders_tests import *
+from admin_integration_tests import *
---
+++ tests/core/tests/admin_integration_tests.py
@@ -0,0 +1,53 @@
+import os.path
+
+from django.test.testcases import TestCase
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+
+
+class ImportExportAdminIntegrationTest(TestCase):
+
+    def setUp(self):
+        user = User.objects.create_user('admin', 'admin@example.com',
+                'password')
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        self.client.login(username='admin', password='password')
+
+    def test_import_export_template(self):
+        response = self.client.get('/admin/core/book/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,
+                'admin/import_export/change_list_import_export.html')
+        self.assertContains(response, _('Import'))
+        self.assertContains(response, _('Export'))
+
+    def test_import(self):
+        input_format = 'tablib.formats._csv'
+        filename = os.path.join(
+                os.path.dirname(__file__),
+                os.path.pardir,
+                'exports',
+                'books.csv')
+        data = {
+                'input_format': input_format,
+                'import_file': open(filename),
+                }
+        response = self.client.post('/admin/core/book/import/', data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('result', response.context)
+        self.assertFalse(response.context['result'].has_errors())
+        self.assertIn('confirm_form', response.context)
+        confirm_form = response.context['confirm_form']
+
+        data = confirm_form.initial
+        response = self.client.post('/admin/core/book/process_import/', data,
+                follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, _('Import finished'))
+
+    def test_export(self):
+        response = self.client.get('/admin/core/book/export/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.has_header("Content-Disposition"))
