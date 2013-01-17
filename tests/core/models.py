79afdfd571dc173a71bb6ad4680832d6cdd956c3
Ngo Minh Nam <emoinrp@gmail.com>
2013-01-17 15:32:03 +1100

    Use field_name to get  widget_kwargs_for_field in fields that follow relationship. Tests attached.

--- tests/core/models.py
+++ tests/core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    birthday = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return self.name
