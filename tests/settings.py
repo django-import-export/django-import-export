cf4359b5af6939a7dade3e94bee689e6afe7c1d8
Bojan Mihelac <bmihelac@mihelac.org>
2013-11-29 10:40:34 +0100

    Update tests to use Django 1.6 test runner
    See https://docs.djangoproject.com/en/1.6/releases/1.6/#new-test-runner
    Also set DATABASE["TEST_NAME"]

--- tests/settings.py
+++ tests/settings.py
@@ -27,7 +27,7 @@ if os.environ.get('IMPORT_EXPORT_TEST_TYPE') == 'mysql-innodb':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'import_export_test',
+            'TEST_NAME': 'import_export_test',
             'USER': os.environ.get('IMPORT_EXPORT_MYSQL_USER', 'root'),
             'OPTIONS': {
                'init_command': 'SET storage_engine=INNODB',
