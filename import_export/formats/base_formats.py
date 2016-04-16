191f81ffed47e2486fa97dfab40fb63b25d794e4
Gunnar Thielebein <gunnar.thielebein@etomer.com>
2016-04-16 16:44:24 +0200

    * add **kwargs to export_data / create_dataset for passing further arguments to csvwriter

--- import_export/formats/base_formats.py
+++ import_export/formats/base_formats.py
@@ -53,7 +53,7 @@ class Format(object):
         """
         raise NotImplementedError()
 
-    def export_data(self, dataset):
+    def export_data(self, dataset, **kwargs):
         """
         Returns format representation for given dataset.
         """
@@ -102,13 +102,13 @@ class TablibFormat(Format):
     def get_title(self):
         return self.get_format().title
 
-    def create_dataset(self, in_stream):
+    def create_dataset(self, in_stream, **kwargs):
         data = tablib.Dataset()
-        self.get_format().import_set(data, in_stream)
+        self.get_format().import_set(data, in_stream, **kwargs)
         return data
 
-    def export_data(self, dataset):
-        return self.get_format().export_set(dataset)
+    def export_data(self, dataset, **kwargs):
+        return self.get_format().export_set(dataset, **kwargs)
 
     def get_extension(self):
         # we support both 'extentions' and 'extensions' because currently
@@ -141,11 +141,11 @@ class CSV(TextFormat):
     TABLIB_MODULE = 'tablib.formats._csv'
     CONTENT_TYPE = 'text/csv'
 
-    def create_dataset(self, in_stream):
+    def create_dataset(self, in_stream, **kwargs):
         if sys.version_info[0] < 3:
             # python 2.7 csv does not do unicode
-            return super(CSV, self).create_dataset(in_stream.encode('utf-8'))
-        return super(CSV, self).create_dataset(in_stream)
+            return super(CSV, self).create_dataset(in_stream.encode('utf-8'), **kwargs)
+        return super(CSV, self).create_dataset(in_stream, **kwargs)
 
 
 class JSON(TextFormat):
