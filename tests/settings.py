4f6f03074db50ff39707b75e93cff341efe43223
Jon Dufresne <jon.dufresne@gmail.com>
2018-11-04 16:24:54 -0800

    Run SQLite tests with in-memory database
    Using an in-memory datbase is faster and doesn't require storing a large
    binary file in the repository.

--- tests/settings.py
+++ tests/settings.py
@@ -78,7 +78,6 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(os.path.dirname(__file__), 'database.db'),
         }
     }
 
