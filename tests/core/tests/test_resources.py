0663dc89162d92f3159bc089f658eb3cd96a66fc
Andy Babic <ababic@rkh.co.uk>
2018-11-05 23:40:52 +0000

    And another one!

--- tests/core/tests/test_resources.py
+++ tests/core/tests/test_resources.py
@@ -324,7 +324,7 @@ class ModelResourceTest(TestCase):
         self.assertTrue(result.rows[0].errors)
         actual = result.rows[0].errors[0].error
         self.assertIsInstance(actual, ValueError)
-        self.assertIn("Column 'id': could not convert string to float", str(actual))
+        self.assertIn("could not convert string to float", str(actual))
 
     def test_import_data_delete(self):
 
