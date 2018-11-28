7e794fe3365f103a16db7a2e98b558efe5f7fe8b
Andy Babic <ababic@rkh.co.uk>
2018-11-28 00:28:43 +0000

    Unit test fixes

--- tests/core/tests/test_invalidrow.py
+++ tests/core/tests/test_invalidrow.py
@@ -10,31 +10,28 @@ class InvalidRowTest(TestCase):
 
     def setUp(self):
         # Create a ValidationEror with a mix of field-specific and non-field-specific errors
-        e = ValidationError('Error one')
-        e.update_error_dict({
-            'name': ['Error two', 'Error three'],
-            'birthday': ['Error four', 'Error five'],
+        non_field_errors = ValidationError(['Error 1', 'Error 2', 'Error 3'])
+        field_errors = ValidationError({
+            'name': ['Error 4', 'Error 5'],
+            'birthday': ['Error 6', 'Error 7'],
         })
+        combined_error_dict = non_field_errors.update_error_dict(field_errors.error_dict)
+        e = ValidationError(combined_error_dict)
         # Use the error to create an InvalidRow instance
         values = {'name': 'ABC', 'birthday': '123'}
         self.obj = InvalidRow(number=1, validation_error=e, values=values)
 
     def test_error_count(self):
-        self.assertEqual(self.obj.error_count(), 5)
+        self.assertEqual(self.obj.error_count(), 7)
 
     def test_non_field_specific_errors(self):
-        self.assertIsInstance(self.obj.non_field_specific_errors, list)
-        self.assertEqual(len(self.obj.non_field_specific_errors), 1)
-        self.assertEqual(self.obj.non_field_specific_errors[0], 'Error one')
+        result = self.obj.non_field_specific_errors
+        self.assertIsInstance(result, list)
+        self.assertEqual(result, ['Error 1', 'Error 2', 'Error 3'])
 
     def test_field_specific_errors(self):
-        self.assertIsInstance(self.obj.field_specific_errors, dict)
-        self.assertEqual(len(self.obj.field_specific_errors), 2)
-        self.assertEqual(
-            self.obj.field_specific_errors['name'],
-            ['Error two', 'Error three']
-        )
-        self.assertEqual(
-            self.obj.field_specific_errors['birthday'],
-            ['Error four', 'Error five']
-        )
+        result = self.obj.field_specific_errors
+        self.assertIsInstance(result, dict)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result['name'], ['Error 4', 'Error 5'])
+        self.assertEqual(result['birthday'], ['Error 6', 'Error 7'])
