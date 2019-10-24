f12e725931db21cb77bd61a828eeb2a9b178cf08
Bastien Vallet <b.vallet@criteo.com>
2019-10-24 16:40:47 +0200

    [django2.2] Add real support of Django 2.2 before 3.0 is out

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ CLASSIFIERS = [
     'Framework :: Django',
     'Framework :: Django :: 2.0',
     'Framework :: Django :: 2.1',
+    'Framework :: Django :: 2.2',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
