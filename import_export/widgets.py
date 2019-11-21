597c448004af92e21034918ef2b13400d13f59cf
Andrey Novikov <novikov@gmail.com>
2019-11-21 15:13:39 +0300

    Initialize Decimal with text value

--- import_export/widgets.py
+++ import_export/widgets.py
@@ -85,7 +85,7 @@ class DecimalWidget(NumberWidget):
     def clean(self, value, row=None, *args, **kwargs):
         if self.is_empty(value):
             return None
-        return Decimal(value)
+        return Decimal(force_text(value))
 
 
 class CharWidget(Widget):
