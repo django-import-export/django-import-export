f665cbe748f46f51069e7dca07dd45a27a4e9edf
Moritz Federspiel <moritz.federspiel@steadysense.at>
2019-12-17 10:08:26 +0100

    Improve tests

--- tests/core/tests/test_resources.py
+++ tests/core/tests/test_resources.py
@@ -316,18 +316,31 @@ class ModelResourceTest(TestCase):
             list(self.resource.iter_queryset(qs))
             mocked_method.assert_called_once_with(chunk_size=1)
 
-    def test_iter_queryset_prefetch(self):
-        paginator = "import_export.resources.Paginator"
-        qs = Book.objects.prefetch_related("categories").all()
-        with mock.patch.object(qs, "order_by") as mocked_method:
-            list(self.resource.iter_queryset(qs))
+    def test_iter_queryset_prefetch_unordered(self):
+        qsu = Book.objects.prefetch_related("categories").all()
+        qso = qsu.order_by('pk').all()
+        with mock.patch.object(qsu, "order_by") as mocked_method:
+            mocked_method.return_value = qso
+            list(self.resource.iter_queryset(qsu))
             mocked_method.assert_called_once_with("pk")
 
-        qs = qs.order_by('pk').all()
-        with mock.patch(paginator, autospec=True) as mocked_obj:
-            mocked_obj.return_value = Paginator(qs, 1)
+    def test_iter_queryset_prefetch_ordered(self):
+        qs = Book.objects.prefetch_related("categories").order_by('pk').all()
+        with mock.patch("import_export.resources.Paginator", autospec=True) as p:
+            p.return_value = Paginator(qs, 1)
             list(self.resource.iter_queryset(qs))
-            mocked_obj.assert_called_once_with(qs, 1)
+            p.assert_called_once_with(qs, 1)
+
+    def test_iter_queryset_prefetch_chunk_size(self):
+        class B(BookResource):
+            class Meta:
+                chunk_size = 1000
+        paginator = "import_export.resources.Paginator"
+        qs = Book.objects.prefetch_related("categories").order_by('pk').all()
+        with mock.patch(paginator, autospec=True) as mocked_obj:
+            mocked_obj.return_value = Paginator(qs, 1000)
+            list(B().iter_queryset(qs))
+            mocked_obj.assert_called_once_with(qs, 1000)
 
     def test_get_diff(self):
         diff = Diff(self.resource, self.book, False)
