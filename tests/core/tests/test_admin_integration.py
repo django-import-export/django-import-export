8895443d19aeff3b5ec7799db076677d753cefe9
matthewhegarty <mrhegarty@gmail.com>
2021-04-20 10:55:12 +0100

    Refactored mixins.py and admin.py to remove duplication (#1268)

--- tests/core/tests/test_admin_integration.py
+++ tests/core/tests/test_admin_integration.py
@@ -1,17 +1,33 @@
 import os.path
 from datetime import datetime
+from unittest import mock
+from unittest.mock import MagicMock
 
-from core.admin import AuthorAdmin, BookAdmin, BookResource, CustomBookAdmin
+from core.admin import (
+    AuthorAdmin,
+    BookAdmin,
+    BookResource,
+    CustomBookAdmin,
+    ImportMixin,
+)
 from core.models import Author, Book, Category, EBook, Parent
 from django.contrib.admin.models import LogEntry
 from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.http import HttpRequest
 from django.test.testcases import TestCase
 from django.test.utils import override_settings
 from django.utils.translation import gettext_lazy as _
 from tablib import Dataset
 
+from import_export.admin import (
+    ExportActionModelAdmin,
+    ExportMixin,
+    ImportExportActionModelAdmin,
+)
 from import_export.formats.base_formats import DEFAULT_FORMATS
+from import_export.tmp_storages import TempFolderStorage
 
 
 class ImportExportAdminIntegrationTest(TestCase):
@@ -328,6 +344,110 @@ class ImportExportAdminIntegrationTest(TestCase):
                 1, 0, EBook._meta.verbose_name_plural)
         )
 
+    def test_get_skip_admin_log_attribute(self):
+        m = ImportMixin()
+        m.skip_admin_log = True
+        self.assertTrue(m.get_skip_admin_log())
+
+    def test_get_tmp_storage_class_attribute(self):
+        """Mock dynamically loading a class defined by an attribute"""
+        target = "SomeClass"
+        m = ImportMixin()
+        m.tmp_storage_class = "tmpClass"
+        with mock.patch("import_export.admin.import_string") as mock_import_string:
+            mock_import_string.return_value = target
+            self.assertEqual(target, m.get_tmp_storage_class())
+
+    def test_get_import_data_kwargs_with_form_kwarg(self):
+        """
+        Test that if a the method is called with a 'form' kwarg,
+        then it is removed and the updated dict is returned
+        """
+        request = MagicMock(spec=HttpRequest)
+        m = ImportMixin()
+        kw = {
+            "a": 1,
+            "form": "some_form"
+        }
+        target = {
+            "a": 1
+        }
+        self.assertEqual(target, m.get_import_data_kwargs(request, **kw))
+
+    def test_get_import_data_kwargs_with_no_form_kwarg_returns_empty_dict(self):
+        """
+        Test that if a the method is called with no 'form' kwarg,
+        then an empty dict is returned
+        """
+        request = MagicMock(spec=HttpRequest)
+        m = ImportMixin()
+        kw = {
+            "a": 1,
+        }
+        target = {}
+        self.assertEqual(target, m.get_import_data_kwargs(request, **kw))
+
+    def test_get_context_data_returns_empty_dict(self):
+        m = ExportMixin()
+        self.assertEqual(dict(), m.get_context_data())
+
+    def test_media_attribute(self):
+        """
+        Test that the 'media' attribute of the ModelAdmin class is overridden to include
+        the project-specific js file.
+        """
+        mock_model = mock.MagicMock()
+        mock_site = mock.MagicMock()
+
+        class TestExportActionModelAdmin(ExportActionModelAdmin):
+            def __init__(self):
+                super().__init__(mock_model, mock_site)
+
+        m = TestExportActionModelAdmin()
+        target_media = m.media
+        self.assertEqual('import_export/action_formats.js', target_media._js[-1])
+
+
+class ImportActionDecodeErrorTest(TestCase):
+    mock_model = mock.Mock(spec=Book)
+    mock_model.__name__ = "mockModel"
+    mock_site = mock.MagicMock()
+    mock_request = MagicMock(spec=HttpRequest)
+    mock_request.POST = {'a': 1}
+    mock_request.FILES = {}
+
+    class TestImportExportActionModelAdmin(ImportExportActionModelAdmin):
+        def __init__(self, mock_model, mock_site, error_instance):
+            self.error_instance = error_instance
+            super().__init__(mock_model, mock_site)
+
+        def write_to_tmp_storage(self, import_file, input_format):
+            mock_storage = MagicMock(spec=TempFolderStorage)
+
+            mock_storage.read.side_effect = self.error_instance
+            return mock_storage
+
+    @mock.patch("import_export.admin.ImportForm")
+    def test_import_action_handles_UnicodeDecodeError(self, mock_form):
+        mock_form.is_valid.return_value = True
+        b_arr = b'\x00\x00'
+        m = self.TestImportExportActionModelAdmin(self.mock_model, self.mock_site,
+                                                  UnicodeDecodeError('codec', b_arr, 1, 2, 'fail!'))
+        res = m.import_action(self.mock_request)
+        self.assertEqual(
+            "<h1>Imported file has a wrong encoding: \'codec\' codec can\'t decode byte 0x00 in position 1: fail!</h1>",
+            res.content.decode())
+
+    @mock.patch("import_export.admin.ImportForm")
+    def test_import_action_handles_error(self, mock_form):
+        mock_form.is_valid.return_value = True
+        m = self.TestImportExportActionModelAdmin(self.mock_model, self.mock_site,
+                                                  ValueError("fail"))
+        res = m.import_action(self.mock_request)
+        self.assertRegex(
+            res.content.decode(),
+            r"<h1>ValueError encountered while trying to read file: .*</h1>")
+
 
 class ExportActionAdminIntegrationTest(TestCase):
 
@@ -366,3 +486,16 @@ class ExportActionAdminIntegrationTest(TestCase):
         }
         response = self.client.post('/admin/core/category/', data)
         self.assertEqual(response.status_code, 302)
+
+    def test_get_export_data_raises_PermissionDenied_when_no_export_permission_assigned(self):
+        request = MagicMock(spec=HttpRequest)
+
+        class TestMixin(ExportMixin):
+            model = Book
+
+            def has_export_permission(self, request):
+                return False
+        m = TestMixin()
+        with self.assertRaises(PermissionDenied):
+            m.get_export_data('0', Book.objects.none(), request=request)
+
