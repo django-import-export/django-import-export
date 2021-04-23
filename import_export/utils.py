722c376e476f0a9b5252c21d4cf788c9bdd1bdd8
VladimirChikunov <bldm95@mail.ru>
2021-04-23 14:06:14 +0300

    Suport multiple databases (#1254)
    Co-authored-by: vchikunov <vchikunov@tradingview.com>

--- import_export/utils.py
+++ import_export/utils.py
@@ -7,14 +7,14 @@ class atomic_if_using_transaction:
     Replaces code::
 
         if using_transactions:
-            with transaction.atomic():
+            with transaction.atomic(using=using):
                 return something()
         return something()
     """
-    def __init__(self, using_transactions):
+    def __init__(self, using_transactions, using):
         self.using_transactions = using_transactions
         if using_transactions:
-            self.context_manager = transaction.atomic()
+            self.context_manager = transaction.atomic(using=using)
 
     def __enter__(self):
         if self.using_transactions:
