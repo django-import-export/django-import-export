082c748de8012d45e1d70d0d3b94b89954af6c10
matthewhegarty <mrhegarty@gmail.com>
2022-12-11 17:07:25 +0000

    added get_import_resource_kwargs() to CustomBookAdmin

--- import_export/resources.py
+++ import_export/resources.py
@@ -268,14 +268,18 @@ class Resource(metaclass=DeclarativeMetaclass):
     representations and handle importing and exporting data.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, **kwargs):
+        """
+        kwargs:
+           An optional dict of kwargs.
+           Subclasses can use kwargs to pass dynamic values to enhance import / exports.
+        """
         # The fields class attribute is the *class-wide* definition of
         # fields. Because a particular *instance* of the class might want to
         # alter self.fields, we create self.fields here by copying cls.fields.
         # Instances should always modify self.fields; they should not modify
         # cls.fields.
         self.fields = deepcopy(self.fields)
-        self.kwargs = kwargs
 
         # lists to hold model instances in memory when bulk operations are enabled
         self.create_instances = list()
