faee7f5f98a9d5a4a1d1c3e643a949bf6a0bc22f
matthewhegarty <mrhegarty@gmail.com>
2023-02-11 17:15:27 +0000

    added default value for get()

--- import_export/admin.py
+++ import_export/admin.py
@@ -502,7 +502,7 @@ class ImportMixin(BaseImportMixin, ImportExportMixinBase):
                 input_format.encoding = self.from_encoding
             import_file = import_form.cleaned_data['import_file']
 
-            if getattr(settings, "IMPORT_EXPORT_SKIP_ADMIN_CONFIRM"):
+            if getattr(settings, "IMPORT_EXPORT_SKIP_ADMIN_CONFIRM", False):
                 # This setting means we are going to skip the import confirmation step.
                 # Go ahead and process the file for import in a transaction
                 # If there are any errors, we roll back the transaction.
