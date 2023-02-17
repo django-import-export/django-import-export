60ba565dda7dd223ca6e928ea27f5165b409f0e8
Jakub Szafrański <s@samu.pl>
2023-02-17 10:32:57 +0100

    Add tests that reproduce XSS vulnerability in HTML export

--- tests/core/tests/test_base_formats.py
+++ tests/core/tests/test_base_formats.py
@@ -205,3 +205,33 @@ class TextFormatTest(TestCase):
 
     def test_is_binary(self):
         self.assertFalse(self.format.is_binary())
+
+
+class HTMLFormatTest(TestCase):
+
+    def setUp(self):
+        self.format = base_formats.HTML()
+        self.dataset = tablib.Dataset(headers=['id', 'username', 'name'])
+        self.dataset.append(('1', 'good_user', 'John Doe'))
+        self.dataset.append(('2', 'evil_user', '<script>alert("I want to steal your credit card data")</script>'))
+
+    def test_export_data(self):
+        res = self.format.export_data(self.dataset)
+        self.assertEqual(
+            (
+                "<table>\n"
+                "<thead>\n"
+                "<tr><th>id</th>\n"
+                "<th>username</th>\n"
+                "<th>name</th></tr>\n"
+                "</thead>\n"
+                "<tr><td>1</td>\n"
+                "<td>good_user</td>\n"
+                "<td>John Doe</td></tr>\n"
+                "<tr><td>2</td>\n"
+                "<td>evil_user</td>\n"
+                "<td>&lt;script&gt;alert(&quot;I want to steal your credit card data&quot;)&lt;/script&gt;</td></tr>\n"
+                "</table>"
+            ),
+            res
+        )
