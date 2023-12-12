f88a0e6b8f737d1a63ea0c9f85ae586accd70685
matthewhegarty <mrhegarty@gmail.com>
2023-12-12 15:14:23 +0000

    removed test code

--- tests/core/admin.py
+++ tests/core/admin.py
@@ -36,9 +36,6 @@ class BookAdmin(ImportExportModelAdmin):
     resource_classes = [BookResource, BookNameResource]
     change_list_template = "core/admin/change_list.html"
 
-    def get_tmp_storage_class_kwargs(self):
-        return {"MEDIA_FOLDER": None}
-
 
 class CategoryAdmin(ExportActionModelAdmin):
     pass
