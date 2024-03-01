aa4796e0b2cad1c28a206ebad50840a8e6b6e49f
Bojan Mihelac <bmihelac@mihelac.org>
2024-03-01 15:32:11 +0100

    fix: remove unused import

--- tests/core/admin.py
+++ tests/core/admin.py
@@ -5,9 +5,7 @@ from import_export.admin import (
     ImportExportModelAdmin,
     ImportMixin,
 )
-from import_export.fields import Field
 from import_export.resources import ModelResource
-from import_export.widgets import DecimalWidget
 
 from .forms import CustomConfirmImportForm, CustomExportForm, CustomImportForm
 from .models import Author, Book, Category, Child, EBook
