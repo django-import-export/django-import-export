7922dcecd4f0910d2819c634286551814dbe3247
Akyl <akyl@gmail.com>
2024-03-13 23:33:11 +0100

    some changes in models

--- tests/core/models.py
+++ tests/core/models.py
@@ -78,9 +78,9 @@ class Book(models.Model):
     author_email = models.EmailField("Author email", max_length=75, blank=True)
     imported = models.BooleanField(default=False)
     published = models.DateField("Published", blank=True, null=True)
-    published_time = models.TimeField("Time published", blank=True, null=True)
+    published_time = models.TimeField("Time published", blank=True, null=True, auto_now_add=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    added = models.DateTimeField(blank=True, null=True)
+    added = models.DateTimeField(blank=True, null=True, auto_now_add=True)
 
     categories = models.ManyToManyField(Category, blank=True)
 
