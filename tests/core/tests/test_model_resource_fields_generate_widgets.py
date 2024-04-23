29acdbda60908c5328de5c35070e54fa50b300b5
mishka251 <mishkabelka251@gmail.com>
2024-04-23 23:59:01 +0500

    Extract method for collecting fields

--- tests/core/tests/test_model_resource_fields_generate_widgets.py
+++ tests/core/tests/test_model_resource_fields_generate_widgets.py
@@ -1,8 +1,12 @@
 from unittest import TestCase
 
 import django
-from django.contrib.postgres import fields as postgres_fields
+from django.contrib.contenttypes import fields as contenttype_fields
+from django.contrib.postgres import fields as postgres
+from django.contrib.postgres import search as postgres_search
+from django.contrib.postgres.fields.ranges import ContinuousRangeField
 from django.db import models
+from django.db.models.fields.related import RelatedField
 
 from import_export import widgets
 from import_export.resources import ModelResource
@@ -27,19 +31,12 @@ class TestImportExportBug(TestCase):
             )
 
     def test_all_db_fields_has_widgets(self):
-        all_django_fields_classes = self._collect_all_clas_children(models.Field)
-        expected_not_presented_fields = {
-            models.ForeignObject,
-            models.ImageField,
-            models.FileField,
-            models.BinaryField,
-            models.FilePathField,
-        }
-        if django.VERSION >= (5, 0):
-            expected_not_presented_fields |= {models.GeneratedField}
-        all_fields = (
-            self._get_default_django_fields() + self._get_postgres_django_fields()
+        all_django_fields_classes = self._get_all_django_model_field_subclasses()
+        expected_has_default_widget = self._get_fields_with_expected_default_widget()
+        expected_not_presented_fields = (
+            self._get_expected_not_presented_in_test_field_subclasses()
         )
+        all_fields = self._get_django_fields_for_check_widget()
 
         field_instance_by_field_cls = {field.__class__: field for field in all_fields}
 
@@ -49,12 +46,25 @@ class TestImportExportBug(TestCase):
                     "test", field, False
                 )
                 widget = resource_field.widget
-                self.assertNotEqual(
-                    widget.__class__.__name__,
-                    "Widget",
-                    msg=f"{field_cls.__name__} has default widget",
-                )
+                if field_cls in expected_has_default_widget:
+                    self.assertEqual(
+                        widget.__class__,
+                        widgets.Widget,
+                        msg=(
+                            f"{field_cls.__name__} "
+                            "expected default widget "
+                            f"actual {widget.__class__}"
+                        ),
+                    )
+                else:
+                    self.assertNotEqual(
+                        widget.__class__,
+                        widgets.Widget,
+                        msg=f"{field_cls.__name__} has default widget class",
+                    )
 
+        # if in new version django will be added new field subclass
+        # this subtest should fail
         for field_cls in all_django_fields_classes:
             if field_cls in expected_not_presented_fields:
                 continue
@@ -65,6 +75,60 @@ class TestImportExportBug(TestCase):
                     msg=f"{field_cls.__name__} not presented in test widgets",
                 )
 
+    def _get_fields_with_expected_default_widget(self):
+        """
+        Returns set of django.db.models.field.Field subclasses
+        which expected has default Widget in ModelResource
+        """
+        expected_has_default_widget = {
+            models.BinaryField,
+            models.FileField,
+            models.FilePathField,
+            models.GenericIPAddressField,
+            models.ImageField,
+            models.IPAddressField,
+            models.SlugField,
+            models.TextField,
+            models.UUIDField,
+            postgres.BigIntegerRangeField,
+            postgres.CICharField,
+            postgres.CIEmailField,
+            postgres.CITextField,
+            postgres.DateRangeField,
+            postgres.DateTimeRangeField,
+            postgres.DecimalRangeField,
+            postgres.HStoreField,
+            postgres.IntegerRangeField,
+            postgres.RangeField,
+        }
+        return expected_has_default_widget
+
+    def _get_expected_not_presented_in_test_field_subclasses(self):
+        """
+        Return set of django.db.models.field.Field subclasses
+        which expected NOT presented in this test in
+         _get_django_fields_for_check_widget
+        """
+        expected_not_presented_fields = {
+            contenttype_fields.GenericRelation,
+            ContinuousRangeField,
+            models.ForeignObject,
+            postgres_search._Float4Field,
+            postgres_search.SearchQueryField,
+            postgres_search.SearchVectorField,
+            RelatedField,
+        }
+
+        if django.VERSION >= (5, 0):
+            expected_not_presented_fields |= {models.GeneratedField}
+        return expected_not_presented_fields
+
+    def _get_all_django_model_field_subclasses(self):
+        """
+        returns list of classes - all subclasses for django.db.models.field.Field
+        """
+        return self._collect_all_clas_children(models.Field)
+
     def _collect_all_clas_children(self, cls):
         children = []
         for child_cls in cls.__subclasses__():
@@ -72,55 +136,57 @@ class TestImportExportBug(TestCase):
             children.extend(self._collect_all_clas_children(child_cls))
         return children
 
-    def _get_default_django_fields(self):
+    def _get_django_fields_for_check_widget(self):
+        """
+        Return list of field instances for all checking field classes
+        """
         fields = [
-            models.PositiveBigIntegerField(),
-            models.PositiveSmallIntegerField(),
-            models.ManyToManyField(WithPositiveIntegerFields),
-            models.OneToOneField(WithPositiveIntegerFields, on_delete=models.PROTECT),
-            models.ForeignKey(WithPositiveIntegerFields, on_delete=models.PROTECT),
-            models.JSONField(),
-            models.UUIDField(),
-            models.TimeField(),
-            models.TextField(),
-            models.GenericIPAddressField(),
-            models.IPAddressField(),
-            models.OrderWrt(),
             models.AutoField(),
-            models.PositiveIntegerField(),
-            models.SmallAutoField(),
-            models.PositiveSmallIntegerField(),
-            models.SmallIntegerField(),
             models.BigAutoField(),
-            models.PositiveBigIntegerField(),
             models.BigIntegerField(),
-            models.IntegerField(),
-            models.FloatField(),
-            models.DurationField(),
-            models.DecimalField(),
-            models.DateTimeField(),
-            models.DateField(),
-            models.URLField(),
-            models.SlugField(),
-            models.EmailField(),
-            models.CommaSeparatedIntegerField(),
-            models.CharField(),
-            models.NullBooleanField(),
+            models.BinaryField(),
             models.BooleanField(),
+            models.CharField(),
+            models.CommaSeparatedIntegerField(),
+            models.DateField(),
+            models.DateTimeField(),
+            models.DecimalField(),
+            models.DurationField(),
+            models.EmailField(),
+            models.FileField(),
+            models.FilePathField(),
+            models.FloatField(),
+            models.ForeignKey(WithPositiveIntegerFields, on_delete=models.PROTECT),
+            models.GenericIPAddressField(),
+            models.ImageField(),
+            models.IntegerField(),
+            models.IPAddressField(),
+            models.JSONField(),
+            models.ManyToManyField(WithPositiveIntegerFields),
+            models.NullBooleanField(),
+            models.OneToOneField(WithPositiveIntegerFields, on_delete=models.PROTECT),
+            models.OrderWrt(),
+            models.PositiveBigIntegerField(),
+            models.PositiveIntegerField(),
+            models.PositiveSmallIntegerField(),
+            models.SlugField(),
+            models.SmallAutoField(),
+            models.SmallIntegerField(),
+            models.TextField(),
+            models.TimeField(),
+            models.URLField(),
+            models.UUIDField(),
+            postgres.ArrayField(models.CharField),
+            postgres.BigIntegerRangeField(),
+            postgres.CICharField(),
+            postgres.CIEmailField(),
+            postgres.CITextField(),
+            postgres.DateRangeField(),
+            postgres.DateTimeRangeField(),
+            postgres.DecimalRangeField(),
+            postgres.HStoreField(),
+            postgres.IntegerRangeField(),
+            postgres.JSONField(),
+            postgres.RangeField(),
         ]
         return fields
-
-    def _get_postgres_django_fields(self):
-        return [
-            postgres_fields.DateRangeField(),
-            postgres_fields.BigIntegerRangeField(),
-            postgres_fields.IntegerRangeField(),
-            postgres_fields.DateTimeRangeField(),
-            postgres_fields.DecimalRangeField(),
-            postgres_fields.RangeField(),
-            postgres_fields.HStoreField(),
-            postgres_fields.ArrayField(models.CharField),
-            postgres_fields.CITextField(),
-            postgres_fields.CICharField(),
-            postgres_fields.CIEmailField(),
-        ]
