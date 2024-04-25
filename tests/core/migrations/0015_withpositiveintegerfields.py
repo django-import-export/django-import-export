47ae9b9730564d01c03f2a76e53139dce8f0fcfd
mishka251 <mishkabelka251@gmail.com>
2024-04-25 22:36:47 +0500

    Fix migration

--- tests/core/migrations/0015_withpositiveintegerfields.py
+++ tests/core/migrations/0015_withpositiveintegerfields.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("core", "0011_uuidcategory_legacybook_alter_uuidbook_id_and_more"),
+        ("core", "0014_bookwithchapternumbers"),
     ]
 
     operations = [
