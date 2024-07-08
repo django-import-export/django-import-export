d5a2a639ac50e045b77bebcacc315d53f800aa6d
matthewhegarty <mrhegarty@gmail.com>
2024-07-08 20:19:21 +0100

    re-added test method

--- tests/core/tests/admin_integration/test_action_export.py
+++ tests/core/tests/admin_integration/test_action_export.py
@@ -152,19 +152,19 @@ class ExportActionAdminIntegrationTest(AdminTestMixin, TestCase):
                 response.content.decode(),
             )
 
-    # def test_export_admin_action_with_restricted_pks_deprecated(self):
-    #     data = {
-    #         "format": "0",
-    #         "export_items": [str(self.cat1.id)],
-    #         **self.resource_fields_payload,
-    #     }
-    #     with self.assertWarnsRegex(
-    #         DeprecationWarning,
-    #         r"The 'get_valid_export_item_pks\(\)' method in "
-    #         "core.admin.CategoryAdmin is deprecated and will be removed "
-    #         "in a future release",
-    #     ):
-    #         self.client.post(self.category_export_url, data)
+    def test_export_admin_action_with_restricted_pks_deprecated(self):
+        data = {
+            "format": "0",
+            "export_items": [str(self.cat1.id)],
+            **self.resource_fields_payload,
+        }
+        with self.assertWarnsRegex(
+            DeprecationWarning,
+            r"The 'get_valid_export_item_pks\(\)' method in "
+            "core.admin.CategoryAdmin is deprecated and will be removed "
+            "in a future release",
+        ):
+            self.client.post(self.category_export_url, data)
 
     def _perform_export_action_calls_modeladmin_get_queryset_test(self, data):
         # Issue #1864
