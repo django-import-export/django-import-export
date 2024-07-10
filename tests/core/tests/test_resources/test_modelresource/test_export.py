727f87c2482c9fc4f8def448b97c0b80769637fb
matthewhegarty <mrhegarty@gmail.com>
2024-07-10 15:27:27 +0100

    simplified test

--- tests/core/tests/test_resources/test_modelresource/test_export.py
+++ tests/core/tests/test_resources/test_modelresource/test_export.py
@@ -119,14 +119,6 @@ class ExportFunctionalityTest(TestCase):
         self.assertEqual("Ian Fleming", dataset.dict[0]["Author Name"])
 
     def test_select_field_raises_ValueError(self):
-        class EBookResource(ModelResource):
-            published = Field(column_name="published_date")
-
-            class Meta:
-                model = EBook
-                fields = ("id", "published_date")
-
-        resource = EBookResource()
         r = "cannot identify field for export with name 'unknown'"
         with self.assertRaisesRegex(ValueError, r):
-            resource._select_field("unknown")
+            self.resource._select_field("unknown")
