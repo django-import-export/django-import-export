775da3cc922a53fc6a06a4745afda3c7367af926
matthewhegarty <mrhegarty@gmail.com>
2024-09-13 18:25:24 +0100

    added constant strings

--- tests/core/tests/test_permissions.py
+++ tests/core/tests/test_permissions.py
@@ -1,12 +1,13 @@
 import os.path
 
+from admin_integration.mixins import AdminTestMixin
 from core.models import Category
 from django.contrib.auth.models import Permission, User
 from django.test.testcases import TestCase
 from django.test.utils import override_settings
 
 
-class ImportExportPermissionTest(TestCase):
+class ImportExportPermissionTest(AdminTestMixin, TestCase):
     def setUp(self):
         user = User.objects.create_user("admin", "admin@example.com", "password")
         user.is_staff = True
@@ -23,7 +24,7 @@ class ImportExportPermissionTest(TestCase):
     @override_settings(IMPORT_EXPORT_IMPORT_PERMISSION_CODE="change")
     def test_import(self):
         # user has no permission to import
-        response = self.client.get("/admin/core/book/import/")
+        response = self.client.get(self.book_import_url)
         self.assertEqual(response.status_code, 403)
 
         # POST the import form
@@ -38,16 +39,16 @@ class ImportExportPermissionTest(TestCase):
                 "import_file": f,
             }
 
-            response = self.client.post("/admin/core/book/import/", data)
+            response = self.client.post(self.book_import_url, data)
             self.assertEqual(response.status_code, 403)
 
-            response = self.client.post("/admin/core/book/process_import/", {})
+            response = self.client.post(self.book_process_import_url, {})
             self.assertEqual(response.status_code, 403)
 
         # user has sufficient permission to import
         self.set_user_model_permission("change", "book")
 
-        response = self.client.get("/admin/core/book/import/")
+        response = self.client.get(self.book_import_url)
         self.assertEqual(response.status_code, 200)
 
         # POST the import form
@@ -62,29 +63,29 @@ class ImportExportPermissionTest(TestCase):
                 "import_file": f,
             }
 
-            response = self.client.post("/admin/core/book/import/", data)
+            response = self.client.post(self.book_import_url, data)
             self.assertEqual(response.status_code, 200)
             confirm_form = response.context["confirm_form"]
 
             data = confirm_form.initial
-            response = self.client.post("/admin/core/book/process_import/", data)
+            response = self.client.post(self.book_process_import_url, data)
             self.assertEqual(response.status_code, 302)
 
     @override_settings(IMPORT_EXPORT_EXPORT_PERMISSION_CODE="change")
     def test_export_with_permission_set(self):
-        response = self.client.get("/admin/core/book/export/")
+        response = self.client.get(self.book_export_url)
         self.assertEqual(response.status_code, 403)
 
         data = {"format": "0"}
-        response = self.client.post("/admin/core/book/export/", data)
+        response = self.client.post(self.book_export_url, data)
         self.assertEqual(response.status_code, 403)
 
         self.set_user_model_permission("change", "book")
-        response = self.client.get("/admin/core/book/export/")
+        response = self.client.get(self.book_export_url)
         self.assertEqual(response.status_code, 200)
 
         data = {"format": "0"}
-        response = self.client.post("/admin/core/book/export/", data)
+        response = self.client.post(self.book_export_url, data)
         self.assertEqual(response.status_code, 200)
 
     @override_settings(IMPORT_EXPORT_EXPORT_PERMISSION_CODE="change")
@@ -94,18 +95,18 @@ class ImportExportPermissionTest(TestCase):
             "action": ["export_admin_action"],
             "_selected_action": [str(self.cat1.id)],
         }
-        response = self.client.post("/admin/core/category/", data)
+        response = self.client.post(self.category_change_url, data)
         self.assertEqual(response.status_code, 403)
 
         self.set_user_model_permission("change", "category")
-        response = self.client.post("/admin/core/category/", data)
+        response = self.client.post(self.category_change_url, data)
         self.assertEqual(response.status_code, 200)
 
     @override_settings(IMPORT_EXPORT_EXPORT_PERMISSION_CODE="add")
     def test_check_export_button(self):
         self.set_user_model_permission("change", "book")
 
-        response = self.client.get("/admin/core/book/")
+        response = self.client.get(self.core_book_url)
         widget = "import_link"
         self.assertIn(widget, response.content.decode())
         widget = "export_link"
@@ -115,7 +116,7 @@ class ImportExportPermissionTest(TestCase):
     def test_check_import_button(self):
         self.set_user_model_permission("change", "book")
 
-        response = self.client.get("/admin/core/book/")
+        response = self.client.get(self.core_book_url)
         widget = "import_link"
         self.assertNotIn(widget, response.content.decode())
         widget = "export_link"
