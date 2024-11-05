643d08ac21ae781a41cc388800c5c937f68af0c5
Bojan Mihelac <bmihelac@mihelac.org>
2024-11-05 13:21:49 +0100

    test: add more tests (#1973)

--- tests/core/tests/test_command_export.py
+++ tests/core/tests/test_command_export.py
@@ -20,3 +20,15 @@ class ExportCommandTest(TestCase):
             data,
             "id,name,author,author_email,imported,published,published_time,price,added,categories\n100,Some book,,,0,,,,,\n",  # noqa
         )
+
+    def test_export_command_as_csv_with_encoding(self):
+        Book.objects.create(id=100, name="Some book")
+
+        call_command("export", "CSV", "core.Book", stdout=self.out, encoding="cp1250")
+
+        self.out.seek(0)
+        data = self.out.read()
+        self.assertEqual(
+            data,
+            "id,name,author,author_email,imported,published,published_time,price,added,categories\n100,Some book,,,0,,,,,\n",  # noqa
+        )
--- tests/core/tests/test_command_import.py
+++ tests/core/tests/test_command_import.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 from django.core.management import call_command
 from django.test import TestCase
 
-from io import BytesIO, StringIO, TextIOWrapper
+from io import StringIO
 from core.models import Book
 
 CSV_CONTENT = """\
@@ -11,16 +11,24 @@ id,name,author,author_email,imported,published,published_time,price,added,catego
 1,Some book updat,,test@example.com,0,,,10.50,,1
 """
 
+CSV_CONTENT_WITH_ERRORS = """\
+id,name,author,author_email,imported,published,published_time,price,added,categories
+Some book updat,,test@example.com,0,,,10.50,,1
+"""
+
 
 class ImportCommandTest(TestCase):
     def setUp(self):
-        self.out = TextIOWrapper(BytesIO())
+        self.out = StringIO()
+        self.err = StringIO()
 
     def test_import_command_with_csv(self):
         with tempfile.NamedTemporaryFile(mode="w+", suffix=".csv") as tmp_csv:
             tmp_csv.write(CSV_CONTENT)
             tmp_csv.seek(0)
-            call_command("import", "core.Book", tmp_csv.name, stdout=self.out)
+            call_command(
+                "import", "core.Book", tmp_csv.name, stdout=self.out, stderr=self.err
+            )
 
         self.assertEqual(Book.objects.count(), 1)
 
@@ -29,6 +37,39 @@ class ImportCommandTest(TestCase):
         mock_stdin.write(CSV_CONTENT)
         mock_stdin.seek(0)
 
-        call_command("import", "core.Book", "-", stdout=self.out, format="CSV")
+        call_command(
+            "import", "core.Book", "-", stdout=self.out, stderr=self.err, format="CSV"
+        )
 
         self.assertEqual(Book.objects.count(), 1)
+
+    def test_import_command_dry_run(self):
+        with tempfile.NamedTemporaryFile(mode="w+", suffix=".csv") as tmp_csv:
+            tmp_csv.write(CSV_CONTENT)
+            tmp_csv.seek(0)
+            call_command(
+                "import",
+                "core.Book",
+                tmp_csv.name,
+                stdout=self.out,
+                stderr=self.err,
+                dry_run=True,
+            )
+
+        self.assertEqual(Book.objects.count(), 0)
+
+    def test_import_command_errors(self):
+        with tempfile.NamedTemporaryFile(mode="w+", suffix=".csv") as tmp_csv:
+            tmp_csv.write(CSV_CONTENT_WITH_ERRORS)
+            tmp_csv.seek(0)
+            with self.assertRaises(SystemExit):
+                call_command(
+                    "import",
+                    "core.Book",
+                    tmp_csv.name,
+                    stdout=self.out,
+                    stderr=self.err,
+                )
+
+        assert "Import errors!" in self.err.getvalue()
+        self.assertEqual(Book.objects.count(), 0)
