d5eea8441d86c527345a3e15ec62d6d5eff35902
Willem Van Onsem <yourfriends@hapytex.eu>
2025-07-15 13:08:04 +0200

    add a test case

--- tests/core/tests/test_resources/test_modelresource/test_resource_factory.py
+++ tests/core/tests/test_resources/test_modelresource/test_resource_factory.py
@@ -9,3 +9,9 @@ class ModelResourceFactoryTest(TestCase):
         BookResource = resources.modelresource_factory(Book)
         self.assertIn("id", BookResource.fields)
         self.assertEqual(BookResource._meta.model, Book)
+
+    def test_create_with_meta(self):
+        BookResource = resources.modelresource_factory(
+            Book, meta_options={"clean_model_instances": True}
+        )
+        self.assertEqual(BookResource._meta.clean_model_instances, True)
