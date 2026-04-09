ae0db83c65ca6a33f94b8c7fb39c814a9858b602
Rodolfo Becerra <rodolvbg@gmail.com>
2026-04-09 13:08:53 +0200

    Enhance CachedForeignKeyWidget test to verify related field caching and query count

--- tests/core/tests/test_widgets.py
+++ tests/core/tests/test_widgets.py
@@ -868,12 +868,18 @@ class CachedForeignKeyWidgetTest(TestCase, RowDeprecationTestMixin):
         )
 
     def test_with_related_fields(self):
-        self.assertEqual(
-            self.book,
-            widgets.CachedForeignKeyWidget(Book, "author__name").clean(
-                self.author.name
-            ),
-        )
+        author2 = Author.objects.create(name="Baz")
+        book = Book.objects.create(name="Baz", author=author2)
+        widget = widgets.CachedForeignKeyWidget(Book, "author__name")
+        with CaptureQueriesContext(connection) as ctx:
+            self.assertEqual(self.book, widget.clean(self.author.name))
+        self.assertEqual(len(ctx.captured_queries), 1)
+        with CaptureQueriesContext(connection) as ctx:
+            self.assertEqual(
+                book,
+                widget.clean(author2.name),
+            )
+        self.assertEqual(len(ctx.captured_queries), 0)
 
 
 class ManyToManyWidget(TestCase, RowDeprecationTestMixin):
